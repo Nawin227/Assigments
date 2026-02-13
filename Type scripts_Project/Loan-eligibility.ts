fe6b2df081a1839a7a060bac31c9206571839830
Naveen kumar <nawinkumar971@gmail.com>
2026-02-13 23:29:44 +0530

    Loan eligbality

---
+++ Type scripts_Project/Loan-eligibility.ts
@@ -0,0 +1,34 @@
+function checkLoanEligibility(custmerName:string,creditscore:number,income:number,isemployed:boolean,debtToIncomeRatio:number):void  
+{
+    if (creditscore >=750){
+        console.log(`${custmerName}-loan approved`);
+        return;
+    }
+    if (creditscore >=650 && creditscore <=750){
+
+        if (income <50000){
+            console.log(`${custmerName}-loan denied`);
+            return;
+        }
+            if(!isemployed){
+                console.log(`${custmerName}-loan denied`);
+                return;
+            }
+           if (debtToIncomeRatio < 40){
+                console.log(`${custmerName}-loan approved`);
+           }
+            else{
+                console.log(`${custmerName}-loan denied`);
+                return;
+                }
+            }
+            }
+            const customerName="Naveen";
+            const creditScore=700;
+            const income=60000;
+            const isEmployed=true;
+            const debtToIncomeRatio=3.5;
+            checkLoanEligibility(customerName,creditScore,income,isEmployed,debtToIncomeRatio);
+
+        
+    
